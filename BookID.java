8c3b82470883f0b5ea4a5302df5108213dc8fc08
JoemariObrial <117140287+JoemariObrial@users.noreply.github.com>
2022-11-01 13:07:48 +0800

    Add files via upload

---
+++ BookID.java
@@ -0,0 +1,14 @@
+package Ernest;
+public class BookID 
+{
+  private int bid;   
+  BookID(int id){
+      this.bid = id;
+  } 
+  public int getbid(){
+      return bid;
+  } 
+  public void setbid(int id){
+      this.bid = id;
+  }
+}
